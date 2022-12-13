c65a3a472cb1ba03879aa6702a97b3edca3e6110
Sofia Rodriguez <95593729+ySofiarodriguez15@users.noreply.github.com>
2022-12-12 22:01:16 -0300

    deploy para railway

---
+++ index.js
@@ -0,0 +1,34 @@
+
+const express = require('express');
+const dotenv = require('dotenv').config();
+const cors = require('cors');
+const PORT = process.env.PORT || 8080;
+
+require('./database/conexion')
+
+const app = express();
+
+const ConsultaControllers = require('./controllers/consultaC.js');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors()); 
+
+app.get('/', async (req, res) => {
+    res.json({
+        consulta: await ConsultaControllers.findAll()
+    }); 
+});
+
+app.post('/crear', async (req, res) => {
+    //console.log(req.body);
+        await ConsultaControllers.create(req.body);
+    res.json('Consulta Creada')
+});
+
+
+
+
+app.listen(PORT, ()=>{
+    console.log(`Servidor corriendo en el Puerto: ${PORT}`);
+})
